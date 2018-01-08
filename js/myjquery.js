d8cb99d80ac807d2515d1bffc424a9ff8c6bcda3
Joseph Park <heeseung5@gmail.com>
2018-01-08 00:41:08 -0500

    Fix mobile responsiveness on navbar

---
+++ js/myjquery.js
@@ -0,0 +1,35 @@
+$( document ).ready(function() {
+	$(".nav-item").addClass("topmenu");
+	$(".topmenu").addClass("ml-4").addClass("mr-4");
+
+	// Tooltip
+	$('button').tooltip({
+	trigger: 'click',
+	placement: 'bottom'
+	});
+
+	function setTooltip(btn, message) {
+	$(btn).tooltip('hide')
+	    .attr('data-original-title', message)
+	    .tooltip('show');
+	}
+
+	function hideTooltip(btn) {
+	setTimeout(function() {
+	    $(btn).tooltip('hide');
+	}, 1000);
+	}
+
+	// Clipboard
+	var clipboard = new Clipboard('.btn');
+
+	clipboard.on('success', function(e) {
+	setTooltip(e.trigger, 'Copied!');
+	hideTooltip(e.trigger);
+	});
+
+	clipboard.on('error', function(e) {
+	setTooltip(e.trigger, 'Failed!');
+	hideTooltip(e.trigger);
+	});
+});
